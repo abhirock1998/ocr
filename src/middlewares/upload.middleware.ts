b5a07b3679d24302509d323ffbdf1f8b7319563d
Jack1998 <yaduvanshiabhi1998@gmail.com>
2024-06-16 22:02:10 +0530

    add cool new feature

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,12 +1,13 @@
 import { uploadFolder } from "@utils/file.util";
 import multer from "multer";
 import fs from "fs";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     console.log(`File is:`, file);
     console.log("Upload folder:", uploadFolder);
-    console.log("COOL:", fs.readdirSync(__dirname));
+    console.log("COOL:", fs.readdirSync(path.join(__dirname, "..")));
     cb(null, uploadFolder);
   },
   filename: (req, file, cb) => {
